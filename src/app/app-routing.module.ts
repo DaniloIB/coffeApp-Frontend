e3c88c0bcb1c60357a03c25d52ef96092f74553e
Danilo Ibañez <ibaniez.danilo@gmail.com>
2022-11-07 13:08:48 -0300

    Primera carga del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HomeComponent } from './component/home/home.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: DashboardComponent, 
+  children: [
+    {path: "menu", component: MenuComponent},
+    {path: "", component: HomeComponent},
+
+  ]
+},
+{path: '', redirectTo: '/home', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { HomeComponent } from './component/home/home.component';
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
